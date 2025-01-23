24c0f094a41b7809d44250dbfb786a01dda72abf
yste7454 <sandrine.bailloux@orange.com>
2025-01-23 10:34:51 +0100

    extract css, and add column in home for travel cards

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,12 +1,156 @@
-let url = "travel_recommendation_api.json";
-let searchbar= document.getElementById("search");
-let searchbutton= document.getElementById("searchBtn");
+const decoLine = document.getElementById("line")
+const socialMediaIcons = document.querySelectorAll("#social-media-sidebar>img")
+const navbarHome = document.getElementById("home-btn")
+const navbarAbout = document.getElementById("about-btn")
+const navbarContact = document.getElementById("contact-btn")
+const current = document.getElementsByClassName("show")
+const home = document.getElementById("home")
+const about = document.getElementById("about")
+const contact = document.getElementById("contact")
+const recommendationsResult = document.getElementById("recommendations")
+const searchResults = document.getElementById("search-result")
+const searchInput = document.getElementById("travel-search")
+const searchBtn = document.getElementById("searchBtn")
+const clearSearch = document.getElementById("resetBtn")
+const form = document.getElementsByTagName("form")
+const bookbtn = document.getElementById("booknow-btn")
+let retrievedData;
+
+function clearRecommendations(){
+    const recoCards = document.getElementsByClassName("reco-card")
+    Array.from(recoCards).forEach(card => card.remove()) 
+    searchResults.childNodes[0].textContent = ""
+
+}
+
+clearSearch.addEventListener("click", clearRecommendations)
+
+async function recommendations(){
+    try {
+        const response = await fetch("./travel_recommendation_api.json",{method:"GET"})
+        if(response.ok){
+            retrievedData = await response.json()
+        }
+    } catch (error) {
+        if(error){
+            return; 
+        }
+    }
+}
+
+
+searchInput.addEventListener("click", async (e) => {
+    if(!retrievedData){
+        await recommendations()
+    }
+    e.target.value = ""
+
+})
+searchInput.addEventListener("blur", (e) => {
+    e.target.value = "Enter a keyword"
+    searchResults.childNodes[0].textContent = ""
+    searchResults.childNodes[0].style.padding = "0"
+})
+
+
+searchInput.addEventListener("input", (e) => {
+    if(retrievedData){
+        searchResults.childNodes[0].textContent = "Explore all"
+        for(const d of Object.keys(retrievedData)){
+            if (d.toLocaleLowerCase().match(e.target.value.toLowerCase())){
+                searchResults.childNodes[0].style.padding = "1rem"
+                searchResults.childNodes[0].textContent = d
+            }
+        }
+            const cities = Object.values(retrievedData.countries)
+            cities.forEach(city =>  {
+                if(city.name.toLowerCase().match(e.target.value.toLowerCase())){
+                searchResults.childNodes[0].style.padding = "1rem"
+                    searchResults.childNodes[0].textContent = city.name
+                }
+            })  
+    }
+    
+})
+
+searchInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter"){
+        const Evt = new MouseEvent("mousedown")
+        searchResults.childNodes[0].dispatchEvent(Evt)
+        const Blur = new Event("blur")
+        searchInput.dispatchEvent(Blur)
+
+    }
+})
+
+function addRecommendations(rdata){
+    clearRecommendations()
+    searchResults.childNodes[0].textContent = ""
+    for(const data of rdata){
+        const card = document.createElement("div")
+        card.classList.add("reco-card")
+        const image = document.createElement("div")
+        image.style.backgroundImage = `url(${data.imageUrl})`
+        image.classList.add("card-image")
+        const title = document.createElement("h2")
+        title.textContent = data.name
+        const description = document.createElement("p");
+        description.textContent = data.description
+        const date = document.createElement("p")
+        const options = { timeZone: `${data.date}`, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+        /*const cityDate = new Date().toLocaleTimeString('en-US', options);
+         date.textContent = cityDate */
+        const btn = document.createElement("button")
+        btn.addEventListener("click", () => {
+            current[0].classList.remove("show")
+            contact.classList.add("show")   
+        })
+        btn.textContent = "Visit"
+        card.append(image, date,title, description, btn)
+        recommendationsResult.appendChild(card)
+    }
+    return
+}
+
+
+searchResults.childNodes[0].addEventListener("mousedown", (e) => {
+    const value = e.target.textContent
+    current[0].classList.remove("show")
+    home.classList.add("show")  
+    if(value === "Explore all" || value === "countries" || !value){
+        const newArray = []
+        if(value !== "countries"){
+            newArray.push(retrievedData.temples, retrievedData.beaches)
+        }
+        const cities = Object.values(retrievedData.countries)
+        cities.forEach(city =>  {
+             newArray.push(city.cities)
+        })  
+        return addRecommendations(newArray.flat())
+    }
+    if( value === "temples" || value === "beaches" ){
+        addRecommendations(retrievedData[value])
+    }
+    else{
+        const cities = Object.values(retrievedData.countries)
+        cities.forEach(city =>  {
+            if(city.name.match(value)){
+             return addRecommendations(city.cities)
+            }
+        })  
+
+    }
+   return
+})
+
+
+/*
 function search(){
-    if (!searchbar.value){
+    if (!sbar.value){
         clear();
         return;
     }
-    let val = searchbar.value.toLowerCase();
+    let val = sbar.value.toLowerCase();
     if (val ==='beach' || val === 'beaches') val ="beaches";
     else if (val === 'country' || val === 'countries') val ="countries";
     else if (val === 'temple' || val === 'temples') val ="temples";
@@ -19,48 +163,189 @@ function search(){
     .then(response=>response.json())
     .then(data=>{
         let result = data[val];
-        let element = document.getElementById('result');
-        let inh = `<div style = flexible-content">`;
-        if (val === "countries"){
-            result.forEach(item=>{
-                inh+=`<div style = row">`
-                inh+=`<br>`
-                inh+=`<h1>Country:${item["name"]}<h1>`
-                let cities = item["cities"]
-                cities.forEach(i=>{
-                    inh+=`<img src=${i["imageUrl"]} style="max-width: 400px; height:auto">`;
-                    inh+=`<h3>City:${i["name"]}<h3>`
-                    inh+=`<h5>${i["description"]}</h5>`
-                    inh+=`<button style="height:50px;width:150px; color: white; background-color: grey; border-radius: 5px; border: none;">VISIT</button>`
-                    inh+=`</div>`
+        
+        result.forEach(item=>{
+                const card = document.createElement("div")
+                card.classList.add("reco-card")
+                const image = document.createElement("div")
+                image.style.backgroundImage = `url(${data.imageUrl})`
+                image.classList.add("card-image")
+                const title = document.createElement("h2")
+                title.textContent = data.name
+                const description = document.createElement("p");
+                description.textContent = data.description
+                const date = document.createElement("p")
+                const options = { timeZone: `${data.date}`, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+                const btn = document.createElement("button")
+                btn.addEventListener("click", () => {
+                    current[0].classList.remove("show")
+                    contact.classList.add("show")   
                 })
-                inh+=`<br>`
-            })
-        }
-        else{
-            result.forEach(i=>{
-                inh+=`<div style = row">`
-                inh+=`<br>`
-                inh+=`<img src=${i["imageUrl"]} style="max-width: 400px; height:auto">`;
-                inh+=`<h3>City:${i["name"]}<h3>`
-                inh+=`<h5>${i["description"]}</h5>`
-                inh+=`<button style="height:50px;width:150px; color: white; background-color: grey; border-radius: 5px; border: none;">VISIT</button>`
-                inh+=`</div>`
-                })
-        }
-
-        element.innerHTML = inh;
-        element.style.display = "block";
-        element.ent.style.alignSelf = "center"
-
-        document.getElementById("not").style.display="none";
+                btn.textContent = "Visit"
+                card.append(image, date,title, description, btn)
+                recommendationsResult.appendChild(card)
+        })
+        
+        //document.getElementById("not").style.display="none";
     })
 }
 function clear(){
     document.getElementById('result').innerHTML = '';
     document.getElementById('result').style.display="none";
-    document.getElementById("not").style.display="block";
-    searchbar.value=""
+    sbar.value=""
 }
-searchbutton.addEventListener('click', search);
+sbtn.addEventListener('click', search);
 document.getElementById("resetBtn").addEventListener('click', clear);
+
+*/
+/*
+const decoLine = document.getElementById("line")
+const socialMediaIcons = document.querySelectorAll("#social-media-sidebar>img")
+const navbarHome = document.getElementById("home-btn")
+const navbarAbout = document.getElementById("about-btn")
+const navbarContact = document.getElementById("contact-btn")
+const current = document.getElementsByClassName("show")
+const home = document.getElementById("home")
+const about = document.getElementById("about")
+const contact = document.getElementById("contact")
+const recommendationsResult = document.getElementById("recommendations")
+const searchResults = document.getElementById("search-result")
+const searchInput = document.getElementById("searchBtn")
+const clearSearch = document.getElementById("resetBtn")
+const form = document.getElementsByTagName("form")
+const bookbtn = document.getElementById("booknow-btn")
+let retrievedData;
+
+
+
+
+function clearRecommendations(){
+    const recoCards = document.getElementsByClassName("reco-card")
+    Array.from(recoCards).forEach(card => card.remove()) 
+    searchResults.childNodes[0].textContent = ""
+
+}
+
+clearSearch.addEventListener("click", clearRecommendations)
+
+async function recommendations(){
+    try {
+        const response = await fetch("./travel_recommendation_api.json",{method:"GET"})
+        if(response.ok){
+            retrievedData = await response.json()
+        }
+    } catch (error) {
+        if(error){
+            return; 
+        }
+    }
+}
+
+
+
+searchInput.addEventListener("click", async (e) => {
+    if(!retrievedData){
+        await recommendations()
+    }
+    e.target.value = ""
+
+})
+searchInput.addEventListener("blur", (e) => {
+    e.target.value = "Enter destination or keyword"
+    searchResults.childNodes[0].textContent = ""
+    searchResults.childNodes[0].style.padding = "0"
+})
+
+
+searchInput.addEventListener("input", (e) => {
+    if(retrievedData){
+        searchResults.childNodes[0].textContent = "Explore all"
+        for(const d of Object.keys(retrievedData)){
+            if (d.toLocaleLowerCase().match(e.target.value.toLowerCase())){
+                searchResults.childNodes[0].style.padding = "1rem"
+                searchResults.childNodes[0].textContent = d
+            }
+        }
+            const cities = Object.values(retrievedData.countries)
+            cities.forEach(city =>  {
+                if(city.name.toLowerCase().match(e.target.value.toLowerCase())){
+                searchResults.childNodes[0].style.padding = "1rem"
+                    searchResults.childNodes[0].textContent = city.name
+                }
+            })  
+    }
+    
+})
+
+searchInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter"){
+        const Evt = new MouseEvent("mousedown")
+        searchResults.childNodes[0].dispatchEvent(Evt)
+        const Blur = new Event("blur")
+        searchInput.dispatchEvent(Blur)
+
+    }
+})
+
+function addRecommendations(rdata){
+    clearRecommendations()
+    searchResults.childNodes[0].textContent = ""
+    for(const data of rdata){
+        const card = document.createElement("div")
+        card.classList.add("reco-card")
+        const image = document.createElement("div")
+        image.style.backgroundImage = `url(${data.imageUrl})`
+        image.classList.add("card-image")
+        const title = document.createElement("h2")
+        title.textContent = data.name
+        const description = document.createElement("p");
+        description.textContent = data.description
+        const date = document.createElement("p")
+        const options = { timeZone: `${data.date}`, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+        const cityDate = new Date().toLocaleTimeString('en-US', options);
+         date.textContent = cityDate 
+        const btn = document.createElement("button")
+        btn.addEventListener("click", () => {
+            current[0].classList.remove("show")
+            contact.classList.add("show")   
+        })
+        btn.textContent = "Visit"
+        card.append(image, date,title, description, btn)
+        recommendationsResult.appendChild(card)
+    }
+    return
+}
+
+
+searchResults.childNodes[0].addEventListener("mousedown", (e) => {
+    const value = e.target.textContent
+    current[0].classList.remove("show")
+    home.classList.add("show")  
+    if(value === "Explore all" || value === "countries" || !value){
+        const newArray = []
+        if(value !== "countries"){
+            newArray.push(retrievedData.temples, retrievedData.beaches)
+        }
+        const cities = Object.values(retrievedData.countries)
+        cities.forEach(city =>  {
+             newArray.push(city.cities)
+        })  
+        return addRecommendations(newArray.flat())
+    }
+    if( value === "temples" || value === "beaches" ){
+        addRecommendations(retrievedData[value])
+    }
+    else{
+        const cities = Object.values(retrievedData.countries)
+        cities.forEach(city =>  {
+            if(city.name.match(value)){
+             return addRecommendations(city.cities)
+            }
+        })  
+
+    }
+   return
+})*/
+
+
+
